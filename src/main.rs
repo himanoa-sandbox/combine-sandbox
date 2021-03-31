4260285e18dc8f84317ee4373dbd03aad425a95c
himanoa <matsunoappy@gmail.com>
2021-03-31 12:03:11 +0900

    Rename combinators name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use combine::*;
-use combine::{many1, token, Parser,count_min_max} ;
-use combine::parser::char::spaces;
+use combine::{many1, token, Parser} ;
 use combine::error::ParseError;
 
 
@@ -74,6 +73,7 @@ pub enum Block {
 pub enum Inline {
     // Paragraph section
     Value(String),
+    Br,
     Literal { children: Box<Inline> },
     Footnote { kind: FootnoteType, children: Box<Inline> },
     Lead { children: Box<Inline> },
@@ -112,43 +112,43 @@ pub enum Inline {
 
 
 parser!{
-    fn expr[Input]()(Input) -> Block
+    fn document[Input]()(Input) -> Block
     where [Input: Stream<Token = char>]
     {
-        block_expr()
+        block()
     }
 }
 
-pub fn block_expr<Input>() -> impl Parser<Input, Output = Block>
+pub fn block<Input>() -> impl Parser<Input, Output = Block>
     where Input: Stream<Token = char>,
           Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    heading_expr()
+    heading_block()
 }
 
-pub fn inline_expr<Input>() -> impl Parser<Input, Output = Inline>
+pub fn inline<Input>() -> impl Parser<Input, Output = Inline>
     where Input: Stream<Token = char>,
         Input::Error: ParseError<Input::Token, Input::Range, Input::Position>
 {
-    value_expr()
+    value()
 }
 
 
-pub fn value_expr<Input>() -> impl Parser<Input, Output = Inline>
+pub fn value<Input>() -> impl Parser<Input, Output = Inline>
     where Input: Stream<Token = char>,
           Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
     return many1::<String, _, _>(any()).map(|s| Inline::Value(s))
 }
 
-pub fn heading_expr<Input>() -> impl Parser<Input, Output = Block>
+pub fn heading_block<Input>() -> impl Parser<Input, Output = Block>
     where Input: Stream<Token = char>,
           Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
     
         many1::<Vec<char>, _, _>(token('='))
             .and(many1::<String, _, _>(token(' ')))
-            .and(inline_expr())
+            .and(inline())
             .map(|((heading, spaces), children)| {
                 if heading.len() > 5 {
                     let heading_raw = heading.iter().collect::<String>();
@@ -179,7 +179,7 @@ mod tests {
 
     #[test]
     fn test_parse_heading() {
-        let (actual, _) = expr().parse("= Heading").unwrap();
+        let (actual, _) = document().parse("= Heading").unwrap();
         assert_eq!(
             actual,
             Block::Heading {
@@ -189,7 +189,7 @@ mod tests {
             }
         );
 
-        let (actual, _) = expr().parse("== Heading").unwrap();
+        let (actual, _) = document().parse("== Heading").unwrap();
         assert_eq!(
             actual,
             Block::Heading {
@@ -199,7 +199,7 @@ mod tests {
             }
         );
 
-        let (actual, _) = expr().parse("=== Heading").unwrap();
+        let (actual, _) = document().parse("=== Heading").unwrap();
         assert_eq!(
             actual,
             Block::Heading {
@@ -209,7 +209,7 @@ mod tests {
             }
         );
 
-        let (actual, _) = expr().parse("==== Heading").unwrap();
+        let (actual, _) = document().parse("==== Heading").unwrap();
         assert_eq!(
             actual,
             Block::Heading {
@@ -220,7 +220,7 @@ mod tests {
         );
 
 
-        let (actual, _) = expr().parse("===== Heading").unwrap();
+        let (actual, _) = document().parse("===== Heading").unwrap();
         assert_eq!(
             actual,
             Block::Heading {
@@ -230,7 +230,7 @@ mod tests {
             }
         );
 
-        let (actual, _) = expr().parse("====== Heading").unwrap();
+        let (actual, _) = document().parse("====== Heading").unwrap();
         assert_eq!(
             actual,
             Block::Paragraph {
