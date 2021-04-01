2e4101247327ed758ad6db53dbd7e6bc9c7fc4a4
himanoa <matsunoappy@gmail.com>
2021-04-01 09:57:10 +0900

    Exec cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use combine::*;
-use combine::{many1, token, Parser} ;
 use combine::error::ParseError;
-
+use combine::*;
+use combine::{many1, token, Parser};
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum HeadingLevel {
@@ -27,7 +26,7 @@ pub enum FootnoteType {
     Tip,
     Important,
     Warning,
-    Caution
+    Caution,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -36,37 +35,74 @@ pub enum VideoProvider {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct TableColumn { 
-    name: String
+pub struct TableColumn {
+    name: String,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct TableRow { 
-    children: Box<Block>
+pub struct TableRow {
+    children: Box<Block>,
 }
-
-
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Block {
-    Paragraph { children: Vec<Inline> },
-    Heading { level: HeadingLevel, children: Vec<Inline>, id: Option<String>},
+    Paragraph {
+        children: Vec<Inline>,
+    },
+    Heading {
+        level: HeadingLevel,
+        children: Vec<Inline>,
+        id: Option<String>,
+    },
     // Horizontal ruled line section
     HorizontalRuledLine,
     NextPage,
     // List section
-    UnorderdList { children: Vec<Inline> },
-    UnorderdListItem { level: ListLevel, children: Vec<Inline> },
-    CheckList { children: Vec<Inline> },
-    CheclListItem { level: ListLevel, children: Vec<Inline>, checked: bool },
-    OrderdList { children: Vec<Inline> },
-    OrderdListItem { level: ListLevel, children: Vec<Inline> },
-    Label { children:Vec<Inline>, key: Vec<Inline> },
-    Qanda { question: Vec<Inline>, answer:Vec<Inline> },
-    CodeBlock { children: Vec<Inline>, title: Option<String>, file_type: Option<String> },
+    UnorderdList {
+        children: Vec<Inline>,
+    },
+    UnorderdListItem {
+        level: ListLevel,
+        children: Vec<Inline>,
+    },
+    CheckList {
+        children: Vec<Inline>,
+    },
+    CheclListItem {
+        level: ListLevel,
+        children: Vec<Inline>,
+        checked: bool,
+    },
+    OrderdList {
+        children: Vec<Inline>,
+    },
+    OrderdListItem {
+        level: ListLevel,
+        children: Vec<Inline>,
+    },
+    Label {
+        children: Vec<Inline>,
+        key: Vec<Inline>,
+    },
+    Qanda {
+        question: Vec<Inline>,
+        answer: Vec<Inline>,
+    },
+    CodeBlock {
+        children: Vec<Inline>,
+        title: Option<String>,
+        file_type: Option<String>,
+    },
     // Unsupport CodeBlockWithSpeachBaloon
-    Block { children: Vec<Inline>, title: Option<Vec<Inline>> },
-    Table { columns: Vec<TableColumn>, rows: Vec<TableRow>, title: Option<String> }
+    Block {
+        children: Vec<Inline>,
+        title: Option<Vec<Inline>>,
+    },
+    Table {
+        columns: Vec<TableColumn>,
+        rows: Vec<TableRow>,
+        title: Option<String>,
+    },
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -74,18 +110,38 @@ pub enum Inline {
     // Paragraph section
     Value(String),
     Br,
-    Literal { children: Box<Inline> },
-    Footnote { kind: FootnoteType, children: Box<Inline> },
-    Lead { children: Box<Inline> },
+    Literal {
+        children: Box<Inline>,
+    },
+    Footnote {
+        kind: FootnoteType,
+        children: Box<Inline>,
+    },
+    Lead {
+        children: Box<Inline>,
+    },
     // Text format section
-
-    Bold { children: Box<Inline> },
-    Italic { children: Box<Inline> },
-    Monospace { children: Box<Inline> },
-    Marker { children: Box<Inline> },
-    Underline { children: Box<Inline> },
-    LineThrough { children: Box<Inline> },
-    Big { children: Box<Inline> },
+    Bold {
+        children: Box<Inline>,
+    },
+    Italic {
+        children: Box<Inline>,
+    },
+    Monospace {
+        children: Box<Inline>,
+    },
+    Marker {
+        children: Box<Inline>,
+    },
+    Underline {
+        children: Box<Inline>,
+    },
+    LineThrough {
+        children: Box<Inline>,
+    },
+    Big {
+        children: Box<Inline>,
+    },
     // Unsupport Superscript
     // Unsupport Subscript
     // Unsupport Curvequote
@@ -93,25 +149,40 @@ pub enum Inline {
 
     // Label section
     // Link section
-    Link { href: String, children: Box<Inline> },
-    Mail { to: String, children: Box<Inline> },
+    Link {
+        href: String,
+        children: Box<Inline>,
+    },
+    Mail {
+        to: String,
+        children: Box<Inline>,
+    },
     // Unsupport LinkWithAttribute
     // Unsupport InlineAnchor
     // Unsupport InnerCrossReference
     // Unsupport DocumentCrossReference
-    
+
     // Image section
-    Image { src:String, caption:Option<String> },
-    InlineImage { src:String, caption:Option<String> },
+    Image {
+        src: String,
+        caption: Option<String>,
+    },
+    InlineImage {
+        src: String,
+        caption: Option<String>,
+    },
     // Video section
-    Video { id:String, provider: VideoProvider },
+    Video {
+        id: String,
+        provider: VideoProvider,
+    },
     // Code section
-    InlineCode { children: Box<Inline> },
+    InlineCode {
+        children: Box<Inline>,
+    },
 }
 
-
-
-parser!{
+parser! {
     fn document[Input]()(Input) -> Block
     where [Input: Stream<Token = char>]
     {
@@ -120,58 +191,65 @@ parser!{
 }
 
 pub fn block<Input>() -> impl Parser<Input, Output = Block>
-    where Input: Stream<Token = char>,
-          Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
     heading_block()
 }
 
 pub fn inline<Input>() -> impl Parser<Input, Output = Inline>
-    where Input: Stream<Token = char>,
-        Input::Error: ParseError<Input::Token, Input::Range, Input::Position>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
     value()
 }
 
-
 pub fn value<Input>() -> impl Parser<Input, Output = Inline>
-    where Input: Stream<Token = char>,
-          Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    return many1::<String, _, _>(any()).map(|s| Inline::Value(s))
+    return many1::<String, _, _>(any()).map(|s| Inline::Value(s));
 }
 
 pub fn heading_block<Input>() -> impl Parser<Input, Output = Block>
-    where Input: Stream<Token = char>,
-          Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    
-        many1::<Vec<char>, _, _>(token('='))
-            .and(many1::<String, _, _>(token(' ')))
-            .and(inline())
-            .map(|((heading, spaces), children)| {
-                if heading.len() > 5 {
-                    let heading_raw = heading.iter().collect::<String>();
-                    return Block::Paragraph { children: vec![Inline::Value(heading_raw + spaces.as_str()), children] };
-                }
-                let level = match heading.len() {
-                    1 => HeadingLevel::Title,
-                    2 => HeadingLevel::Level1,
-                    3 => HeadingLevel::Level2,
-                    4 => HeadingLevel::Level3,
-                    5 => HeadingLevel::Level4,
-                    _ => {
-                        unreachable!()
-                    },
+    many1::<Vec<char>, _, _>(token('='))
+        .and(many1::<String, _, _>(token(' ')))
+        .and(inline())
+        .map(|((heading, spaces), children)| {
+            if heading.len() > 5 {
+                let heading_raw = heading.iter().collect::<String>();
+                return Block::Paragraph {
+                    children: vec![Inline::Value(heading_raw + spaces.as_str()), children],
                 };
-                Block::Heading { id: None, children: vec![children], level }
-            })
+            }
+            let level = match heading.len() {
+                1 => HeadingLevel::Title,
+                2 => HeadingLevel::Level1,
+                3 => HeadingLevel::Level2,
+                4 => HeadingLevel::Level3,
+                5 => HeadingLevel::Level4,
+                _ => {
+                    unreachable!()
+                }
+            };
+            Block::Heading {
+                id: None,
+                children: vec![children],
+                level,
+            }
+        })
 }
 
 fn main() -> () {
     dbg!("{:?}", "hello");
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -219,7 +297,6 @@ mod tests {
             }
         );
 
-
         let (actual, _) = document().parse("===== Heading").unwrap();
         assert_eq!(
             actual,
@@ -234,7 +311,10 @@ mod tests {
         assert_eq!(
             actual,
             Block::Paragraph {
-                children: vec![Inline::Value("====== ".to_string()), Inline::Value("Heading".to_string())],
+                children: vec![
+                    Inline::Value("====== ".to_string()),
+                    Inline::Value("Heading".to_string())
+                ],
             }
         );
     }
